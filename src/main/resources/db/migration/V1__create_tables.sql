57da1076c3e3365a919bdb39611687b7d68f4574
asya-khachatryan <asya_khachatryan2@edu.aua.am>
2023-11-03 17:47:42 +0400

    fixed migrations

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE users
 (
-    ID         serial PRIMARY KEY NOT NULL,
-    username   VARCHAR(50)        NOT NULL UNIQUE,
-    first_name VARCHAR(50)        NOT NULL,
-    last_name  VARCHAR(50)        NOT NULL,
+    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username   VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
     city       VARCHAR(100),
     balance    NUMERIC(25, 5)
 );
 
 CREATE TABLE transactions
 (
-    ID               serial PRIMARY KEY NOT NULL,
-    sender_user_id   INT                NOT NULL REFERENCES users (ID),
-    receiver_user_id INT                NOT NULL REFERENCES users (ID),
-    amount           NUMERIC(25, 5)     NOT NULL,
-    timestamp        TIMESTAMP
+    id               BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sender_user_id   BIGINT         NOT NULL REFERENCES users (id),
+    receiver_user_id BIGINT         NOT NULL REFERENCES users (id),
+    amount           NUMERIC(25, 5) NOT NULL,
+    creation_time    TIMESTAMP      NOT NULL DEFAULT now()
 )
